4e7849e9abfd3566bf01ff4c7f7e3d5ce064f57a
Fzzy <fzzysoda@gmail.com>
2022-10-31 12:03:51 -0700

    ajfnsdgfdjhnfg

---
+++ src/main/kotlin/ApexEventReader.kt
@@ -0,0 +1,103 @@
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.File
+import java.nio.file.FileSystems
+import java.nio.file.StandardWatchEventKinds
+import java.nio.file.WatchKey
+
+object ApexEventReader {
+
+    class ApexMatch(val filePath: String) {
+
+        private val reader = File(filePath).bufferedReader()
+        private var eventIndex = 0L
+
+        fun ingestEvents(live: Boolean) {
+            while (true) {
+                var line = reader.readLine() ?: break
+                //if (!live) continue
+                if (line.endsWith(",")) line = line.substring(0, line.length - 1)
+                try {
+                    val json = JSONObject(line)
+                    json.put("matchId", File(filePath).nameWithoutExtension)
+                    json.put("eventId", eventIndex++)
+
+                    //println(json.toString())
+                    sendEventToServer(json.toString())
+                    //broadcastEvent(json.toString())
+                    Thread.sleep(5000)
+                } catch (_: JSONException) {
+                }
+            }
+        }
+    }
+
+    val liveapiFolder = File("C:\\Users\\Fzzy\\Saved Games\\Respawn\\Apex\\assets\\temp\\live_api")
+
+    val matches = arrayListOf<ApexMatch>()
+
+    val fileSizes = hashMapOf<String, Long>()
+
+    fun start() {
+        for (file in liveapiFolder.listFiles()!!) {
+            getMatch(file)
+        }
+        Thread {
+            while (true) {
+                fun checkFile(file: File) {
+                    val length = file.length()
+                    val size = fileSizes.getOrDefault(file.absolutePath, length)
+                    if (size != length) {
+                        val m = getMatch(file)
+                        m.ingestEvents(true)
+                    }
+
+                    fileSizes[file.absolutePath] = length
+                }
+
+                for (file in liveapiFolder.listFiles()!!) {
+                    checkFile(file)
+                }
+            }
+        }.start()
+
+        val watchService = FileSystems.getDefault().newWatchService()
+        liveapiFolder.toPath().register(watchService, StandardWatchEventKinds.ENTRY_CREATE)
+        var key: WatchKey?
+        do {
+            key = watchService.take()
+            for (event in key.pollEvents()) {
+                val file = File(liveapiFolder, event.context().toString())
+                if (!matchExists(file)) {
+                    println("new match found!")
+                    Thread.sleep(100)
+                    getMatch(file).ingestEvents(true)
+                }
+                break
+            }
+            key.reset()
+        } while (key != null)
+    }
+
+    fun matchExists(file: File): Boolean {
+        for (m in matches) {
+            if (m.filePath == file.absolutePath) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun getMatch(file: File): ApexMatch {
+        for (m in matches) {
+            if (m.filePath == file.absolutePath) {
+                return m
+            }
+        }
+        val match = ApexMatch(file.absolutePath)
+        matches.add(match)
+        match.ingestEvents(false)
+        println("processed new match: ${file.name}")
+        return match
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+import java.io.DataOutputStream
+import java.net.*
+
+var socketOut: DataOutputStream? = null
+
+fun main(args: Array<String>) {
+    awaitSocketAddress()
+
+    ApexEventReader.start()
+}
+
+fun sendEventToServer(content: String) {
+    try {
+        socketOut?.writeUTF(content)
+    } catch (e: SocketException) {
+        println(">>> Connection to Server Lost.")
+        socketOut = null
+        Thread { awaitSocketAddress() }.start()
+    }
+}
+
+fun awaitSocketAddress() {
+    println(">>> Awaiting discovery packet...")
+    val socket = DatagramSocket(8880, InetAddress.getByName("0.0.0.0"))
+    socket.broadcast = true
+
+    val recvBuf = ByteArray(15000)
+    val packet = DatagramPacket(recvBuf, recvBuf.size)
+
+    while (true) {
+        socket.receive(packet)
+
+        println(">>> Packet received; data: ${String(packet.data).trim()}")
+
+        val message = String(packet.data).trim()
+        if (message.startsWith("APEXREADER_SERVER_BROADCAST")) {
+            val eventSocket = Socket(packet.address, 8884)
+            socketOut = DataOutputStream(eventSocket.getOutputStream())
+
+            println(">>> Address Found: ${packet.address}")
+            break
+        }
+    }
+}
